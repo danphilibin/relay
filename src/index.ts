354a2aaf1f3bb7bc6f4e6345594448fbbd2376e1
Dan Philibin <dan@cornerhealth.com>
2026-01-06 08:53:11 +0000

    update file path

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export { RelayDurableObject, RelayWorkflow };
 export default { fetch: httpHandler };
 
 // Import workflows to trigger self-registration
-import "@/workflows/fetch-hackernews";
+import "@/workflows/fetch-hacker-news";
 import "@/workflows/process-files";
 import "@/workflows/ask-name";
 import "@/workflows/newsletter-signup";
