4089ecd11ca720ffd6c7eac911c5c881e5a2a93d
Dan Philibin <dan@danphilibin.com>
2026-02-25 00:25:44 +0000

    remove process-files

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ export default { fetch: httpHandler };
 
 // Import workflows to trigger self-registration
 import "@/workflows/fetch-hacker-news";
-import "@/workflows/process-files";
 import "@/workflows/ask-name";
 import "@/workflows/newsletter-signup";
 import "@/workflows/survey";
